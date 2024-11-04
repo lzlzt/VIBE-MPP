419b660556d885a6864c003d486621a3eb9b7e4f
lzlzt <114655354+lzlzt@users.noreply.github.com>
2024-11-04 12:01:20 +0800

    Update utils.py

--- finetune/utils.py
+++ finetune/utils.py
@@ -8,7 +8,7 @@ from torch_geometric.data import Batch
 from torch_geometric.data import Data
 
 
-def get_atom_poses(mol, conf):     #最直接地获得3D坐标的方法
+def get_atom_poses(mol, conf):   
         """tbd"""
         atom_poses = []
         for i, atom in enumerate(mol.GetAtoms()):
@@ -18,37 +18,6 @@ def get_atom_poses(mol, conf):     #最直接地获得3D坐标的方法
             atom_poses.append([pos.x, pos.y, pos.z])
         return atom_poses
 
-
-def get_steady_mol(smiles,num):
-    try:
-        mol = AllChem.MolFromSmiles(smiles)
-        new_mol = Chem.AddHs(mol)
-        res = AllChem.EmbedMultipleConfs(new_mol, numConfs=num)
-        res = AllChem.MMFFOptimizeMoleculeConfs(new_mol)
-        new_mol = Chem.RemoveHs(new_mol)
-        index = np.argmin([x[1] for x in res])
-        conf = new_mol.GetConformer(id=int(index))
-    except:
-        try:
-            mol = AllChem.MolFromSmiles(smiles)
-            new_mol = Chem.AddHs(mol)
-            res = AllChem.EmbedMultipleConfs(mol, numConfs=num,useRandomCoords=True)
-            #这个参数可以让RDKIT无法处理的大分子也能获得一个返回值，有效避免程序报错
-            res = AllChem.MMFFOptimizeMoleculeConfs(mol)
-            new_mol = Chem.RemoveHs(new_mol)
-            index = np.argmin([x[1] for x in res])
-            conf = mol.GetConformer(id=int(index))
-        except:
-            mol = AllChem.MolFromSmiles(smiles)
-            new_mol = Chem.AddHs(mol)
-            res = AllChem.EmbedMolecule(new_mol)
-            #这个函数的返回结果为整数，而上面的那种获得异构体的形式返回的是元组，相当于index这一步用这种方法的话就不适用了
-            # res = AllChem.EmbedMultipleConfs(new_mol, numConfs=num)
-            # res = AllChem.MMFFOptimizeMoleculeConfs(new_mol)
-            new_mol = Chem.RemoveHs(new_mol)
-            # index = np.argmin([x[1] for x in res])
-            conf = new_mol.GetConformer(id=int(res))
-    return new_mol,conf
 
 def get_distance(mol,conf):
     num_atoms = mol.GetNumAtoms()
@@ -62,6 +31,50 @@ def get_distance(mol,conf):
             dist = (dist_x**2 + dist_y**2 + dist_z**2) ** 0.5
             distances[i, j] = distances[j, i] = round(dist,3)
     return distances
+
+def get_steady_mol(smiles,num):
+    try:
+        mol = AllChem.MolFromSmiles(smiles)
+        new_mol = Chem.AddHs(mol)
+        res = AllChem.EmbedMultipleConfs(new_mol, numConfs=num)
+        res = AllChem.MMFFOptimizeMoleculeConfs(new_mol)
+        new_mol = Chem.RemoveHs(new_mol)
+        index = np.argmin([x[1] for x in res])
+        conf = new_mol.GetConformer(id=int(index))
+        distances = get_distance(new_mol,conf)
+    except:
+        try:
+            mol = AllChem.MolFromSmiles(smiles)
+            new_mol = Chem.AddHs(mol)
+            res = AllChem.EmbedMultipleConfs(mol, numConfs=num,useRandomCoords=True)
+            res = AllChem.MMFFOptimizeMoleculeConfs(mol)
+            new_mol = Chem.RemoveHs(new_mol)
+            index = np.argmin([x[1] for x in res])
+            conf = mol.GetConformer(id=int(index))
+            distances = get_distance(new_mol,conf)
+        except:
+            try:
+                mol = AllChem.MolFromSmiles(smiles)
+                new_mol = Chem.AddHs(mol)
+                res = AllChem.EmbedMolecule(new_mol)
+                new_mol = Chem.RemoveHs(new_mol)
+                conf = new_mol.GetConformer(id=int(res))
+                distances = get_distance(new_mol,conf)
+            except:
+                try:
+                    mol = Chem.MolFromSmiles(smiles)
+                    new_mol = Chem.AddHs(mol)
+                    AllChem.Compute2DCoords(new_mol) 
+                    new_mol = Chem.RemoveHs(new_mol)
+                    conf = new_mol.GetConformer()
+                    distances = get_distance(new_mol,conf)
+                except:
+                    mol = Chem.MolFromSmiles(smiles)
+                    new_mol = Chem.AddHs(mol)
+                    new_mol = Chem.RemoveHs(new_mol)
+                    num_atoms = new_mol.GetNumAtoms()
+                    distances = np.zeros((num_atoms, num_atoms))              
+    return new_mol,distances
 
 
 def copy_atom(atom):
@@ -98,3 +111,11 @@ def molgraph_to_graph_data(batch):
     new_batch = Batch().from_data_list(graph_data_batch)
     return new_batch
 
+def is_ring(mol, atom_idx1, atom_idx2):
+    ring_info = mol.GetRingInfo()
+    atom_rings = ring_info.AtomRings()
+    for ring in atom_rings:
+        if all(mol.GetAtomWithIdx(atom_idx).GetIsAromatic() for atom_idx in ring):
+            if atom_idx1 in ring and atom_idx2 in ring:
+                return True
+    return False
